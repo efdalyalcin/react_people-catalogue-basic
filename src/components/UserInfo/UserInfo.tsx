7da8ab0689f6c77677e3d64e535e06129967404c
efdalyalcin <efdalyalcin@hotmail.com>
2022-07-20 22:06:10 +0300

    font-family arranged, mobile flex direction changed

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -19,12 +19,12 @@ export const UserInfo: React.FC<Props> = ({
 }) => (
   <div>
     <h2 className="User__info-title">{userName}</h2>
-    <p className="User__info-text"><b>Email:</b>{` ${userEmail}`}</p>
-    <p className="User__info-text"><b>Phone:</b>{` ${userPhone}`}</p>
-    <p className="User__info-text"><b>Company:</b>{` ${userCompany}`}</p>
-    <p className="User__info-text"><b>Website:</b>{` ${userWebsite}`}</p>
+    <p className="User__info-text"><strong>Email:</strong>{` ${userEmail}`}</p>
+    <p className="User__info-text"><strong>Phone:</strong>{` ${userPhone}`}</p>
+    <p className="User__info-text"><strong>Company:</strong>{` ${userCompany}`}</p>
+    <p className="User__info-text"><strong>Website:</strong>{` ${userWebsite}`}</p>
     <p className="User__info-text">
-      <b>Address:</b>
+      <strong>Address:</strong>
       {` ${userAddress.street}, ${userAddress.suite}, `
         + `${userAddress.city}, ${userAddress.zipcode}`}
     </p>
